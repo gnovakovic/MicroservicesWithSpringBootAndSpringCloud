bd24a3602d2a7bea83021aabc47c4e4230ca81a2
gnovakovic <gnovakovic@gmail.com>
2022-12-24 07:12:19 +0100

    Ch13. Distributed Tracing with Sleuth and Zipkin, OK.

--- student-service/src/main/java/com/infybuzz/service/StudentService.java
+++ student-service/src/main/java/com/infybuzz/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.infybuzz.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,23 +10,22 @@ import com.infybuzz.entity.Student;
 import com.infybuzz.feignclients.AddressFeignClient;
 import com.infybuzz.repository.StudentRepository;
 import com.infybuzz.request.CreateStudentRequest;
-import com.infybuzz.response.AddressResponse;
 import com.infybuzz.response.StudentResponse;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class StudentService {
 
+	Logger logger = LoggerFactory.getLogger(StudentService.class);
+
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	@Autowired
 	WebClient webClient;
-	
+
 	@Autowired
 	AddressFeignClient addressFeignClient;
-	
+
 	@Autowired
 	CommonService commonService;
 
@@ -34,30 +35,32 @@ public class StudentService {
 		student.setFirstName(createStudentRequest.getFirstName());
 		student.setLastName(createStudentRequest.getLastName());
 		student.setEmail(createStudentRequest.getEmail());
-		
+
 		student.setAddressId(createStudentRequest.getAddressId());
 		student = studentRepository.save(student);
-		
+
 		StudentResponse studentResponse = new StudentResponse(student);
-		
-		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
-		
+
+		// studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+
 		studentResponse.setAddressResponse(commonService.getAddressById(student.getAddressId()));
 
 		return studentResponse;
 	}
-	
-	public StudentResponse getById (long id) {
+
+	public StudentResponse getById(long id) {
+		logger.info("Inside Student - getById");
+		
 		Student student = studentRepository.findById(id).get();
 		StudentResponse studentResponse = new StudentResponse(student);
-		
-		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
-		
+
+		// studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+
 		studentResponse.setAddressResponse(commonService.getAddressById(student.getAddressId()));
-		
+
 		return studentResponse;
 	}
-	
+
 	/*
 	 * @CircuitBreaker(name = "addressService", fallbackMethod =
 	 * "fallbackGetAddressById") public AddressResponse getAddressById (long
